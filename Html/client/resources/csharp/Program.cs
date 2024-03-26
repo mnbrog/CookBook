1df728a0743d9a816b9749713471a29fe5ca4e55
unknown <mnbrogan32@gmail.com>
2024-03-25 19:44:53 -0500

    hh

--- Html/client/resources/csharp/Program.cs
+++ Html/client/resources/csharp/Program.cs
@@ -1,12 +1,1 @@
-﻿// In Program.cs
-[HttpGet]
-public async Task<IActionResult> GetRecipes() {
-    // Logic to fetch all recipes
-}
-
-[HttpPost]
-public async Task<IActionResult> AddRecipe([FromBody]Recipe recipe) {
-    // Logic to add a new recipe
-}
-
-// ... Other controller methods
+﻿﻿
